3439d2c363221e5005d140ad8533ce2501b2f290
Rafal Lewandkow <r.lewandkow@gmail.com>
2024-03-26 15:22:56 +0100

    Update main_window.py

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-write .bmp file in greyscale
+
 
 @author
 """
@@ -8,6 +8,8 @@ write .bmp file in greyscale
 import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
+
+import data_analysis.converters
 
 def browse_path():
     if folder_var.get():
@@ -26,29 +28,35 @@ def display_s94_files(folder_path):
     for file in s94_files:
         file_listbox.insert(tk.END, file)
 
-root = tk.Tk()
-root.title("File Converter")
+def show_main_window():
+    root = tk.Tk()
+    root.title("File Converter")
 
-# Entry field for file/folder path
-path_entry = tk.Entry(root, width=50)
-path_entry.grid(row=0, column=0, padx=5, pady=5)
+    # Entry field for file/folder path
+    global path_entry  # declare path_entry as global variable
+    path_entry = tk.Entry(root, width=50)
+    path_entry.grid(row=0, column=0, padx=5, pady=5)
 
-# Button to browse for file/folder
-browse_button = tk.Button(root, text="Browse", command=browse_path)
-browse_button.grid(row=0, column=1, padx=5, pady=5)
+    # Checkbox to toggle file/folder mode
+    global folder_var  # declare folder_var as global variable
+    folder_var = tk.BooleanVar()
+    folder_var.set(False)
+    folder_checkbox = tk.Checkbutton(root, text="Convert folder", variable=folder_var)
+    folder_checkbox.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
-# Checkbox to toggle file/folder mode
-folder_var = tk.BooleanVar()
-folder_var.set(False)
-folder_checkbox = tk.Checkbutton(root, text="Convert folder", variable=folder_var)
-folder_checkbox.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+    # Button to browse for file/folder
+    browse_button = tk.Button(root, text="Browse", command=browse_path)
+    browse_button.grid(row=0, column=1, padx=5, pady=5)
 
-# # Button to convert file(s)
-# convert_button = tk.Button(root, text="Convert", command=None)
-# convert_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+    # Listbox to display .s94 files
+    global file_listbox  # declare file_listbox as global variable
+    file_listbox = tk.Listbox(root, width=50, height=10)
+    file_listbox.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
-# Listbox to display .s94 files
-file_listbox = tk.Listbox(root, width=50, height=10)
-file_listbox.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+    root.mainloop()
 
-root.mainloop()
+def main():
+    show_main_window()
+
+if __name__ == '__main__':
+    main()
