b4ddb32add4abccf23da55713a79af10d94754bf
Rafal Lewandkow <r.lewandkow@gmail.com>
2024-04-17 18:27:20 +0200

    copy functionality to spots_detection_tab

--- main_window.py
+++ main_window.py
@@ -49,7 +49,7 @@ class App:
         # # Create the third tab: Detection
         self.spots_detection_tab = ttk.Frame(self.notebook)
         self.notebook.add(self.spots_detection_tab, text="Detection")
-        # self.create_spots_detection_tab()
+        self.create_spots_detection_tab()
 
         # Configure grid row and column weights for rescaling
         self.root.grid_rowconfigure(0, weight=1)
@@ -62,7 +62,112 @@ class App:
     ##########################################################################################################
     #### Spots Detection Tab
     ##########################################################################################################
+    def create_spots_detection_tab(self):
+        # Create listbox to display filenames
+        self.data_listbox_detection = tk.Listbox(self.spots_detection_tab, width=20, height=10, selectmode=tk.SINGLE)
+        self.data_listbox_detection.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        self.data_listbox_detection.bind("<<ListboxSelect>>", self.show_data_for_detection)
 
+        # Add a scrollbar for the listbox
+        listbox_scrollbar = tk.Scrollbar(self.spots_detection_tab, orient=tk.VERTICAL)
+        listbox_scrollbar.grid(row=0, column=1, sticky="ns")
+        self.data_listbox_detection.config(yscrollcommand=listbox_scrollbar.set)
+        listbox_scrollbar.config(command=self.data_listbox_detection.yview)
+
+        # Add a canvas to display the data
+        self.data_canvas_detection = tk.Canvas(self.spots_detection_tab, bg="white")
+        self.data_canvas_detection.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
+
+        # Add vertical scrollbar
+        v_scrollbar = tk.Scrollbar(self.spots_detection_tab, orient=tk.VERTICAL, command=self.data_canvas_detection.yview)
+        v_scrollbar.grid(row=0, column=3, sticky="ns")
+        self.data_canvas_detection.configure(yscrollcommand=v_scrollbar.set)
+
+        # Add horizontal scrollbar
+        h_scrollbar = tk.Scrollbar(self.spots_detection_tab, orient=tk.HORIZONTAL, command=self.data_canvas_detection.xview)
+        h_scrollbar.grid(row=1, column=2, sticky="ew")
+        self.data_canvas_detection.configure(xscrollcommand=h_scrollbar.set)
+
+        # Configure row and column weights for rescaling
+        self.spots_detection_tab.grid_rowconfigure(0, weight=1)
+        self.spots_detection_tab.grid_columnconfigure(0, weight=1)
+        self.spots_detection_tab.grid_columnconfigure(2, weight=1)
+
+        # Bind the function to handle resizing events
+        self.spots_detection_tab.bind("<Configure>", self.resize_canvas_detection_scrollregion)
+
+    def resize_canvas_detection_scrollregion(self, event):
+        # Update the scroll region to cover the entire canvas
+        self.data_canvas_detection.config(scrollregion=self.data_canvas_detection.bbox("all"))
+
+    def insert_data_to_detection(self):
+        self.data_listbox_detection.delete(0, tk.END)
+
+        file_ext = self.data[0]['file_name'][-3:]
+        for item in self.data:
+            if file_ext.lower() == "stp" or file_ext.lower() == "s94":
+                filename_only = os.path.basename(item['file_name'])
+                self.data_listbox_detection.insert(tk.END, filename_only)
+            elif file_ext.lower() == "mpp":
+                filename_only = os.path.basename(item['file_name'])
+                for i, frame in enumerate(item['data'], start=1):
+                    frame_name = f"{filename_only}: frame {i}"
+                    self.data_listbox_detection.insert(tk.END, frame_name)
+
+    def show_data_for_detection(self, event):
+        file_ext = self.data[0]['file_name'][-3:]
+        # Get the index of the selected filename
+        selected_index = self.data_listbox_detection.curselection()
+        if selected_index:
+            index = int(selected_index[0])
+            # Get the corresponding data
+            if file_ext.lower() == "stp" or file_ext.lower() == "s94":
+                selected_data = self.data[index]
+                self.display_detection_image(selected_data)
+            elif file_ext.lower() == "mpp":
+                selected_name = self.data_listbox_detection.get(index)
+                parts = selected_name.split(':')
+                mpp_file_name = parts[0].strip()
+                frame_number = int(parts[1].strip().split()[1])
+                mpp_data = None
+                for item in self.data:
+                    filename_only = os.path.basename(item['file_name'])
+                    if filename_only == mpp_file_name:
+                        mpp_data = item
+                        break
+                selected_data = mpp_data['data'][frame_number - 1]
+                self.display_detection_image(selected_data, True)
+
+    def display_detection_image(self, data, mpp=False):
+        # Clear previous data
+        self.data_canvas_detection.delete("all")
+        points = []
+        if mpp:
+            points = data
+        else:
+            points = data['data']
+        
+        # Create a new grayscale image
+        img = Image.new('L', (len(points[0]), len(points)))
+
+        # Normalize the values in data to the range [0, 255]
+        max_z = max(map(max, points))
+        min_z = min(map(min, points))
+        if max_z == min_z:
+            max_z += 1
+        for i in range(len(points)):
+            for j in range(len(points[i])):
+                val = int(255 * (points[i][j] - min_z) / (max_z - min_z))
+                img.putpixel((j, i), val)
+
+        # Convert the PIL image to a Tkinter PhotoImage
+        photo = ImageTk.PhotoImage(img)
+
+        # Display the image on the canvas
+        self.data_canvas_detection.create_image(0, 0, anchor="nw", image=photo)
+
+        # Save a reference to the PhotoImage to prevent garbage collection
+        self.data_canvas_detection.image = photo
     ##########################################################################################################
     #### Noise Analisys Tab
     ##########################################################################################################
@@ -70,7 +175,7 @@ class App:
         # Create listbox to display filenames
         self.data_listbox_analisys = tk.Listbox(self.noise_analisys_tab, width=20, height=10, selectmode=tk.SINGLE)
         self.data_listbox_analisys.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
-        self.data_listbox_analisys.bind("<<ListboxSelect>>", self.show_data)
+        self.data_listbox_analisys.bind("<<ListboxSelect>>", self.show_data_for_analisys)
 
         # Add a scrollbar for the listbox
         listbox_scrollbar = tk.Scrollbar(self.noise_analisys_tab, orient=tk.VERTICAL)
@@ -118,7 +223,7 @@ class App:
                     frame_name = f"{filename_only}: frame {i}"
                     self.data_listbox_analisys.insert(tk.END, frame_name)
 
-    def show_data(self, event):
+    def show_data_for_analisys(self, event):
         file_ext = self.data[0]['file_name'][-3:]
         # Get the index of the selected filename
         selected_index = self.data_listbox_analisys.curselection()
@@ -127,7 +232,7 @@ class App:
             # Get the corresponding data
             if file_ext.lower() == "stp" or file_ext.lower() == "s94":
                 selected_data = self.data[index]
-                self.display_image(selected_data)
+                self.display_analisys_image(selected_data)
             elif file_ext.lower() == "mpp":
                 selected_name = self.data_listbox_analisys.get(index)
                 parts = selected_name.split(':')
@@ -140,9 +245,9 @@ class App:
                         mpp_data = item
                         break
                 selected_data = mpp_data['data'][frame_number - 1]
-                self.display_image(selected_data, True)
+                self.display_analisys_image(selected_data, True)
 
-    def display_image(self, data, mpp=False):
+    def display_analisys_image(self, data, mpp=False):
         # Clear previous data
         self.data_canvas.delete("all")
         points = []
@@ -273,6 +378,8 @@ class App:
 
             self.loaded_files_text.insert(tk.END, "\n".join(loaded_files))
             self.insert_data_to_analisys()
+            self.insert_data_to_detection()
+            
         except Exception as e:
             error_msg = "Error", f"load_all_files: An error occurred while refreshing the listbox: {str(e)}"
             logger.error(error_msg)
@@ -303,6 +410,7 @@ class App:
 
             self.loaded_files_text.insert(tk.END, "\n".join(loaded_files))
             self.insert_data_to_analisys()
+            self.insert_data_to_detection()
         except Exception as e:
             error_msg = "load_selected_files: Error", f"An error occurred while loading selected files: {str(e)}"
             logger.error(error_msg)
